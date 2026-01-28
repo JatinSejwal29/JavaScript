2b7d314af51df02859770aaeb413563f4e53db47
jatin <sejwaljatin6@gmail.com>
2026-01-28 10:51:38 +0530

    js day 7

---
+++ bca web development/script.js
@@ -0,0 +1,10 @@
+let user = {
+    name : "jatin",
+    address :"gurugram",
+    rollno :"2501060122",
+    favcolour : "red"
+}
+
+console.log(user.name)
+console.log(user.rollno )
+console.log(user.name,user.address,user.rollno,user.favcolour)
